2556765b0af581a5b6f91ca12a9d0d4c19e8f5ca
Riya rai <118008485+riya053@users.noreply.github.com>
2023-07-15 17:03:38 +0530

    Create functionusingpointer.cpp

---
+++ functionusingpointer.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void print(int *p)
+{
+    cout<<*p<<endl;
+    cout<<p<<endl;
+}
+int main(){
+    int value=5;
+    int *p=&value;
+    print(p);
+    return 0;
+
+
+    }
+
+output:
+5
+0x61ff18
